08f159fab86a8bd11a48fdafb3b8292389061c49
nithyakala1988 <41789172+nithyakala1988@users.noreply.github.com>
2018-07-27 12:39:45 +0000

    Update HelloWorld.java

--- src/oota/HelloWorld.java
+++ src/oota/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld  {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello Nithya kala");
+		System.out.println("Hello Nithya kala Sudhakar");
 	}
 
 }
